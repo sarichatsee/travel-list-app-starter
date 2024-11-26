6530e2b8a89face7c36684a55884dbc94524916f
fann <22034013@myrp.edu.sg>
2024-11-26 12:53:59 +0800

    Completed first phase

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
+import React from "react";
+
 // Initial packing items
 const initialItems = [
-  { id: 1, description: "Shirt", quantity: 5, packed: false },
+  { id: 1, description: "Shirt", quantity: 5, packed: true },
   { id: 2, description: "Pants", quantity: 2, packed: false },
 ];
 
@@ -9,19 +11,59 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = React.useState("");
+  const [quantity, setQuantity] = React.useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("Form submitted");
+  }
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit} >
       <h3>What do you need to pack?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(e.target.value)}
+      >
+        <option value={1}>1</option>
+        <option value={2}>2</option>
+        <option value={3}>3</option>
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button type="submit">ADD</button>
     </form>
   );
 }
+
+function Item({ packingItem }) {
+  return (
+    <li>
+      <span
+        style={
+          packingItem.packed
+            ? { textDecoration: "line-through" }
+            : {}
+        }
+      >
+        {packingItem.description} ({packingItem.quantity})
+      </span>
+    </li>
+  );
+}
+
 
 function PackingList() {
   return (
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <li>{item.description}</li>
+          <Item packingItem={item} />
         ))}
       </ul>
     </div>
